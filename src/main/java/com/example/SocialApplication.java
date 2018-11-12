910edfa6e2df83d5591cbe65ac8d0c2670d40fa4
Jonathan Hamilton <jonathan_hamilton@me.com>
2018-11-12 14:45:43 -0800

    tried to fix: Modify 'linkedInResource' so that it conforms to the canonical names requirements.

--- src/main/java/com/example/SocialApplication.java
+++ src/main/java/com/example/SocialApplication.java
@@ -102,7 +102,7 @@ public class SocialApplication extends WebSecurityConfigurerAdapter{
 	  
 	  @Bean
 	  @Primary
-	  @ConfigurationProperties("linkedInResource")
+	  @ConfigurationProperties("linked-in-resource")
 	  public ResourceServerProperties linkedInResource() {
 	  return new ResourceServerProperties();
 	  }
